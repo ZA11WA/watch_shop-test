fe418ee48cfbb898c876d6fc4d769536ac8e2a3b
pzawi <p.zawidzki665@gmail.com>
2024-01-02 20:55:12 +0100

    navbar-sm

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -4,7 +4,6 @@ import { Montserrat } from "next/font/google";
 import CartCount from "./CartCount";
 import UserMenu from "./UserMenu";
 import { getCurrentUser } from "@/actions/getCurrentUser";
-import Categories from "./Categories";
 import SearchBar from "./SearchBar";
 import ThemeSwitcher from "../Button/ThemeSwitcher";
 import CategoriesMenu from "./CategoriesMenu";
@@ -17,29 +16,25 @@ const NavBar = async () => {
     <div className="sticky top-0 w-full bg-stone-100 dark:bg-neutral-900 z-30 shadow-md mb-1">
       <div className="py-4 border-b-[1px]">
         <Container>
-            
-            
-          <div className="flex items-center justify-between gap-6 md:gap-0">
-            <div className="flex items-start">
+          <div className="flex flex-wrap items-center justify-between">
+            <div className="flex items-center gap-6 md:gap-12 order-1">
               <CategoriesMenu />
-
+              <Link
+                className={`${montserrat.className} font-bold text-2xl text-neutral-950 dark:text-white uppercase`}
+                href={"/"}
+              >
+                Tempo Zegara
+              </Link>
             </div>
-            <Link
-              className={`${montserrat.className} font-bold text-2xl text-neutral-950 dark:text-white uppercase`}
-              href={"/"}
-            >
-              Tempo Zegara
-            </Link>
-            {
-              <div className="hidden md:block">
-                <SearchBar />
-              </div>
-            }
-            <div className="flex items-center gap-8 md:gap-12 dark:text-white">
+
+            <div className="flex items-center gap-6 md:gap-8 dark:text-white mt-4 md:mt-0 justify-center md:justify-end w-full md:w-auto order-2 md:order-3">
               <ThemeSwitcher />
               <CartCount />
-
               <UserMenu currentUser={currentUser} />
+            </div>
+
+            <div className="flex-grow order-3 md:order-2 pt-2">
+              <SearchBar />
             </div>
           </div>
         </Container>
